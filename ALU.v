0cf5d06649567423691fe118a51ecff5251cf903
NorthernLand <1479400575@qq.com>
2024-05-24 12:43:30 +0800

    Optimize ALU

--- ALU.v
+++ ALU.v
@@ -16,29 +16,19 @@ module ALU (
     always @(posedge clk) begin
         if (~rst) ALUResult <= 32'b0;
         else begin
-            case(ALUOp)
+            casex(ALUOp)
                 2'b00: // load/store
                     ALUResult <= A + B;
                 2'b01: // branch
                     ALUResult <= A - B;
-                2'b10: // register arithmatic: add, sub, and, or
+                2'b1x: // register arithmatic: add, sub, and, or
                     case(funct3)
-                        3'h0: ALUResult <= ((funct7 == 7'h00) ? A + B : A - B); // add, sub
-                        3'h4: ALUResult <= A ^ B; //xor
+                        3'h0: ALUResult <= ((ALUOp == 2'b10 && funct7 == 7'h00) ? A + B : A - B); // add, sub
+                        3'h4: ALUResult <= A ^ B; // xor
                         3'h6: ALUResult <= A | B; // or
                         3'h7: ALUResult <= A & B; // and
                         3'h1: ALUResult <= A << B[4:0]; // sll
                         3'h5: ALUResult <= ((funct7 == 7'h20) ? A >>> B[4:0] : A >> B[4:0]); //srl, sra
-                        default: ALUResult <= 0;
-                    endcase
-                2'b11: // register with immediate: addi, ori, andi
-                    case(funct3)
-                        3'h0: ALUResult <= A + imm32; // addi
-                        3'h4: ALUResult <= A ^ imm32; // xori
-                        3'h6: ALUResult <= A | imm32; // ori
-                        3'h7: ALUResult <= A & imm32; // andi
-                        3'h1: ALUResult <= A << imm32[4:0]; // sll
-                        3'h5: ALUResult <= ((imm32[11:5] == 7'h20) ? A >>> imm32[4:0] : A >> imm32[4:0]); //srli, srai
                         default: ALUResult <= 0;
                     endcase
             endcase
