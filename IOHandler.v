d5597080fe4749817dff7056f13b4868c14bc248
QwQZZZZZ <92218403+QwQZZZZZ@users.noreply.github.com>
2024-05-26 18:25:40 +0800

    Update IOHandler.v

--- IOHandler.v
+++ IOHandler.v
@@ -21,30 +21,34 @@
 
 
 module IOHandler(
-    input [7:0] read_digit, [3:0] bit_width, button, tube, 
+    input [7:0] read_digit, [3:0] bit_width, button,
     input need_read, //is need to read
-    output reg [31:0] write_digit, is_reading //1 means need to wait, 0 means need to continue
+    output reg [31:0] write_digit, reg stall //1 means need to wait, 0 means need to continue
     );
     reg [31:0] input_buffer;
     reg [31:0] cnt;
     
-    initial begin
+    initial begin 
         cnt = 32'h0000;
     end
     always @* begin
-        if(need_read && cnt < bit_width) begin
-            is_reading = 1'b1;
-            if(button == 1'b1) begin //maybe need to debounce
-                input_buffer[cnt*8 +:8] = read_digit;
-                cnt = cnt + 32'h0001;
+        if(need_read == 1'b1 && cnt < bit_width) begin
+            stall = 1'b1;
+            if(button == 1'b1) begin
+                input_buffer[cnt*8 +: 8] = read_digit;
+                cnt = cnt + 1;
+            end 
+            if(cnt == bit_width) begin
+                write_digit = input_buffer;
             end
         end
-        else if(cnt == bit_width) begin
-            is_reading = 1'b0;
-            write_digit = input_buffer;
-        end
-        else if(!need_read) begin
-            cnt = 32'h0000;
+        else if (cnt == bit_width) begin
+            stall = 1'b1;
+            if(button == 1'b1) begin
+                stall = 1'b0;
+                cnt = 32'h0000;
+                input_buffer = cnt;
+            end
         end
     end
 endmodule
